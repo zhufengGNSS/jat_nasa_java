7e45ec975e3cddd764a7f45fba09822bd0475200
wbamford <wbamford@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2006-05-05 20:06:10 +0000

    Added Files to complete closed-loop filter simulation capabilities.  Main file is sim/closedLoopSim.java

---
+++ trunk/jat/measurements/createMeasurements.java
@@ -0,0 +1,58 @@
+package jat.measurements;
+
+import java.io.*;
+import java.util.HashMap;
+import jat.sim.initializer.*;
+import jat.sim.*;
+import jat.matvec.data.*;
+import jat.alg.estimators.*;
+
+
+public class createMeasurements{
+
+	public static String []measurementTypes;
+	public static VectorN measurementValues;
+	public static int numMeasurementTypes;
+	public static MeasurementModel [] mm;
+	
+	public createMeasurements()
+	{
+		//Read in all of the required measurements
+		HashMap hm = closedLoopSim.hm;
+		numMeasurementTypes = initializer.parseInt(hm,"MEAS.types");
+		measurementTypes = new String[numMeasurementTypes];
+		mm = new MeasurementModel [numMeasurementTypes];
+		
+		for(int i = 0;i<numMeasurementTypes;i++)
+		{
+			String meas = "MEAS."+i+".desc";
+			measurementTypes[i] = initializer.parseString(hm,meas);
+			System.out.println(measurementTypes[i]);
+			if(measurementTypes[i].equals("position"))
+			{
+				mm[i] = new stateMeasurementModel();
+				//MeasurementModel pp = new stateMeasurementModel();
+			}
+			else if(measurementTypes[i].equals("range"))
+			{
+				mm[i] = new rangeMeasurementModel();
+			}
+			else
+			{
+				System.out.println("Invalid measurement type.");
+				System.exit(1);
+				
+			}
+		}
+	}
+	
+	public static int getNumberMeasurements()
+	{	
+		return numMeasurementTypes;
+	}
+	
+
+	
+
+}
+
---
+++ trunk/jat/measurements/stateMeasurementModel.java
@@ -0,0 +1,109 @@
+package jat.measurements;
+
+
+import java.util.HashMap;
+
+import jat.sim.*;
+import jat.gps.Visible;
+import jat.matvec.data.*;
+import java.util.Random;
+import jat.alg.estimators.*;
+
+public class stateMeasurementModel implements MeasurementModel{
+	
+	public static VectorN R;
+	public static int numStates;
+	HashMap hm = closedLoopSim.hm;
+	Random generator;
+	
+	public stateMeasurementModel() {
+		/*Add a sleep in here to insure that the Random Number
+		 * Seeds don't allign with any other random number generator
+		 */
+		try {
+			Thread.sleep(20);
+			generator = new Random();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public VectorN getMeasurement()
+	{
+		String tmp;
+		VectorN range = new VectorN(12);
+		double[] truth0 = closedLoopSim.truth[0].sc.get_spacecraft().toStateVector();
+		double[] truth1 = closedLoopSim.truth[1].sc.get_spacecraft().toStateVector();
+		VectorN sat0 = new VectorN(truth0);
+		VectorN sat1 = new VectorN(truth1);
+		range.set(0,sat0);
+		range.set(6,sat1);
+		
+		//Add in the measurement noise read out of the file
+		for(int j = 0; j < 12; j++)
+		{
+			tmp = "MEAS."+0+".R."+j;
+			double R = initializer.parseDouble(hm,tmp);
+			
+			/*Scale the error as Gaussian noise times the 
+			 square of the measurement noise*/
+			range.x[j] += generator.nextGaussian()*R*R; 
+		}	
+		
+		return range;
+	}
+	
+	public VectorN predictMeasurement(VectorN state){
+		
+		VectorN range = new VectorN(12);
+		range = state.get(0,12);
+		
+		
+		return range;
+		
+	}
+	
+	public double observedMinusPredicted(int i, VectorN state){
+		VectorN oMinusC;
+		VectorN pred = predictMeasurement(state);
+		VectorN obs  = getMeasurement();
+		oMinusC      = obs.minus(pred);
+		return oMinusC.get(i);
+	}
+	
+	public double R(int measNumber, int whichState)
+	{
+		String tmp = "MEAS."+measNumber+".R."+whichState;
+		double R = initializer.parseDouble(hm,tmp);
+		return R;
+	}
+	
+	public VectorN H(int whichState, VectorN state)
+	{
+		/*Determine the number of states*/
+		int numStates = initializer.parseInt(hm,"FILTER.states");
+	
+		/*for a Range measurement, the current state has H = 1, all other states H = 0 */
+		VectorN H = new VectorN(numStates);
+		H.set(0.0);
+		H.set(whichState,1.0);
+		return H;
+	}
+
+	public VectorN H(VectorN xref) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double R() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double zPred(int index, double t, VectorN xref) {
+		// TODO Auto-generated method stub
+		return 0;
+	}	
+}
