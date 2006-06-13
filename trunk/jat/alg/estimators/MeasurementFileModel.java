5058e9941bca7d06a530222180d77208fb88c0e4
rcpage3 <rcpage3@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2006-06-13 18:38:38 +0000

    Missed the new definition of the MeasurementFileModel.java interface.

---
+++ trunk/jat/alg/estimators/MeasurementFileModel.java
@@ -0,0 +1,56 @@
+/* JAT: Java Astrodynamics Toolkit
+ *
+ * Copyright (c) 2006 The JAT Project. All rights reserved.
+ *
+ * This file is part of JAT. JAT is free software; you can 
+ * redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software 
+ * Foundation; either version 2 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Emergent Space Technologies
+ * File created by Richard C. Page III 
+ **/
+package jat.alg.estimators;
+
+import jat.matvec.data.VectorN;
+import jat.measurements.ObservationMeasurement;
+
+public interface MeasurementFileModel {
+
+	/**
+	 * Returns the H matrix
+	 * @param xref VectorN containing the current state
+	 * @return H matrix (measurement state relation)
+	 */
+	public VectorN H (ObservationMeasurement obs, VectorN xref);
+
+	/**
+	 * Returns the measurement noise value
+	 * @return measurement noise (sigma^2)
+	 */
+	public double R (ObservationMeasurement obs);
+
+	/**
+	 * Returns the predicted measurement based on the current state
+	 * @param index measurement index
+	 * @param t time of the measurement
+	 * @param xref VectorN with the current state at the measurement time
+	 */
+	public double zPred (ObservationMeasurement obs, int index, double t, VectorN xref);
+
+	/**
+	 * Checks for remaining measurements. 
+	 * True = more measurements left to be processed.
+	 * @param index measurement index.
+	 */
+
+}
