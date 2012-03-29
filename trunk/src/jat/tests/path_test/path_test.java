8a8d76de3f69926f4f64938106d61adb242990a4
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-03-29 03:38:52 +0000

    rename test to tests

---
+++ trunk/src/jat/tests/path_test/path_test.java
@@ -0,0 +1,12 @@
+package jat.tests.path_test;
+
+import jat.core.util.*;
+
+public class path_test
+{
+    public static void main (String [] args) throws Exception
+    {
+        System.out.println(FileUtil.getClassFilePath("jat.test.path_test","path_test"));
+    }
+}
+
