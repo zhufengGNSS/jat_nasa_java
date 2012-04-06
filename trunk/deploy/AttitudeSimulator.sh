a8017ae907293805aa4894de519a41ef985f7312
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-04-06 01:17:21 +0000

    attitude simulator check if Java3D installed, web page with applet

--- trunk/deploy/AttitudeSimulator.sh
+++ trunk/deploy/AttitudeSimulator.sh
@@ -1,5 +1,11 @@
 
-java -cp ../../jat_src.jar:../lib/plot.jar  jat.application.AttitudeSimulator.AttitudeSimulator
+
+export LD_LIBRARY_PATH=/usr/lib/jni
+export LD_LIBRARY_PATH=/usr/lib/jni
+
+ECHO $LD_LIBRARY_PATH=/home/user/Desktop/j3d-1_5_2-linux-i586/lib/i386
+
+java -cp ../../jat_src.jar:../lib/plot.jar:../lib/j3dcore.jar:../lib/j3dutils.jar:../lib/vecmath.jar   jat.application.AttitudeSimulator.AttitudeSimulator
 
 echo press enter
 
