c064e79579b50f4c0ba14f2a17c07c79b44d1326
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-05-02 17:29:00 +0000

    solar system app

---
+++ trunk/src/jat/jat3D/Line3D.java
@@ -0,0 +1,51 @@
+package jat.jat3D;
+
+import javax.media.j3d.LineArray;
+import javax.media.j3d.Shape3D;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Point3f;
+
+public class Line3D extends TransformGroup {
+
+	public Point3f x1, x2;
+
+	public Line3D(Point3f x1, Point3f x2) {
+		super();
+		LineArray a = new LineArray(2, LineArray.COORDINATES | LineArray.COLOR_3);
+
+		a.setCoordinate(0, x1);
+		a.setCoordinate(1, x2);
+		a.setColor(0, Colors.gray);
+		a.setColor(1, Colors.gray);
+		addChild(new Shape3D(a));
+
+	}
+
+	public Line3D() {
+		super();
+		setCapability(ALLOW_TRANSFORM_WRITE);
+		this.setCapability(ALLOW_CHILDREN_WRITE);
+		x1 = new Point3f(0, 0, 0);
+		x2 = new Point3f(1, 1, 1);
+		LineArray a = new LineArray(2, LineArray.COORDINATES | LineArray.COLOR_3);
+		a.setCoordinate(0, x1);
+		a.setCoordinate(1, x2);
+		a.setColor(0, Colors.gold);
+		a.setColor(1, Colors.gray);
+		addChild(new Shape3D(a));
+	}
+
+	public void change(int i) {
+		x1 = new Point3f(0, 0, i);
+		x2 = new Point3f(1, 1, 1);
+		LineArray a = new LineArray(2, LineArray.COORDINATES | LineArray.COLOR_3);
+		a.setCoordinate(0, x1);
+		a.setCoordinate(1, x2);
+		a.setColor(0, Colors.gold);
+		a.setColor(1, Colors.gray);
+		setChild(new Shape3D(a), 0);
+
+	}
+
+}
