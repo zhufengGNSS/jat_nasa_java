3d29914e9602c97d0ffd1b925ddcaa1d1b4798b4
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-05-04 18:46:10 +0000

    mouse behavior

--- src/jat/jat3D/util.java
+++ src/jat/jat3D/util.java
@@ -4,6 +4,8 @@ import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsEnvironment;
 
 import javax.media.j3d.GraphicsConfigTemplate3D;
+import javax.media.j3d.Transform3D;
+import javax.vecmath.Matrix4f;
 
 public class util {
 
@@ -33,7 +35,26 @@ public class util {
 		return true;
 	}
 
-	
+	public static void print(String title, Matrix4f mat) {
+
+		System.out.println(title);
+		System.out.println(mat.m00 + " " + mat.m01 + " " + mat.m02 + " " + mat.m03);
+		System.out.println(mat.m10 + " " + mat.m11 + " " + mat.m12 + " " + mat.m13);
+		System.out.println(mat.m20 + " " + mat.m21 + " " + mat.m22 + " " + mat.m23);
+		System.out.println(mat.m30 + " " + mat.m31 + " " + mat.m32 + " " + mat.m33);
+		System.out.println();
+	}
+
+	public static void print_matrix_of_transform3D(String title, Transform3D t3) {
+		Matrix4f mat = new Matrix4f();
+		t3.get(mat);
+		System.out.println(title);
+		System.out.println(mat.m00 + " " + mat.m01 + " " + mat.m02 + " " + mat.m03);
+		System.out.println(mat.m10 + " " + mat.m11 + " " + mat.m12 + " " + mat.m13);
+		System.out.println(mat.m20 + " " + mat.m21 + " " + mat.m22 + " " + mat.m23);
+		System.out.println(mat.m30 + " " + mat.m31 + " " + mat.m32 + " " + mat.m33);
+		System.out.println();
+	}	
 }
 
 
