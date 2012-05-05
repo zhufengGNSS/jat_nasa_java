83039f1675c9e61b87e57d8c570ce411283b9955
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-05-05 01:01:39 +0000

    move 3D dependencies from core to jat3D

--- src/jat/core/math/CoordTransform.java
+++ src/jat/core/math/CoordTransform.java
@@ -21,9 +21,6 @@
 
 package jat.core.math;
 
-import javax.vecmath.Vector3d;
-import javax.vecmath.Vector3f;
-
 import jat.core.math.matvec.data.VectorN;
 
 /**
@@ -58,32 +55,5 @@ public class CoordTransform {
 	}
 
 
-	//Test: (1.5,1.5,1) -> r=2.345207879911715, theta=1.1302856637901981, phi=0.7853981633974483
-	
-	public static Vector3f Cartesian_to_Spherical(Vector3f coord) {
-		// r, theta, phi
-		if (coord.x == 0)
-			;
-		// coord.x = Math.e.FLT_EPSILON;
-		Vector3f out = new Vector3f();
 
-		out.x = (float) Math.sqrt((coord.x * coord.x) + (coord.y * coord.y) + (coord.z * coord.z));
-		out.y = (float) Math.acos(coord.z / out.x);
-		out.z = (float) Math.atan(coord.y / coord.x);
-		if (coord.x < 0)
-			out.y += Math.PI;
-
-		return out;
-	}
-
-	public static Vector3f Spherical_to_Cartesian(Vector3f coord) {
-		Vector3f out = new Vector3f();
-		double r = coord.x;
-		double theta = coord.y;
-		double phi = coord.z;
-		out.x = (float) (r * Math.sin(theta) * Math.cos(phi));
-		out.y = (float) (r * Math.sin(theta) * Math.sin(phi));
-		out.z = (float) (r * Math.cos(theta));
-		return out;
-	}
 }
