9a077ac80afb5d970b207b9dca00f42b5b626445
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-05-11 23:53:09 +0000

    license

--- trunk/src/jat/jat3D/JatPlot3D.java
+++ trunk/src/jat/jat3D/JatPlot3D.java
@@ -1,5 +1,26 @@
+/* JAT: Java Astrodynamics Toolkit
+ * 
+  Copyright 2012 Tobias Berthold
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+// Original Code under LGPL
+// http://java.freehep.org/freehep-java3d/license.html
+
 package jat.jat3D;
 
+import jat.jat3D.behavior.PlotKeyBehavior;
 import jat.jat3D.behavior.jat_MouseDownUpBehavior;
 import jat.jat3D.behavior.jat_MouseRotate;
 import jat.jat3D.behavior.jat_MouseZoom;
--- trunk/src/jat/jat3D/behavior/PlotKeyBehavior.java
+++ trunk/src/jat/jat3D/behavior/PlotKeyBehavior.java
@@ -1,4 +1,26 @@
-package jat.jat3D;
+/* JAT: Java Astrodynamics Toolkit
+ * 
+  Copyright 2012 Tobias Berthold
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+// Original Code under LGPL
+// http://java.freehep.org/freehep-java3d/license.html
+
+package jat.jat3D.behavior;
+
+import jat.jat3D.CoordTransform3D;
 
 import java.awt.event.KeyEvent;
 
@@ -87,7 +109,7 @@ public class PlotKeyBehavior extends Behavior {
 			System.out.println("plus pressed");
 			jat_zoom(1);
 			break;
-		case KeyEvent.VK_UNDERSCORE:
+		case KeyEvent.VK_MINUS:
 			System.out.println("minus pressed");
 			jat_zoom(-1);
 			break;
