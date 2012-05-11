269bd7cb9dcb6de23c0d29757290c7c082a58d89
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-05-11 23:53:09 +0000

    license

--- src/jat/jat3D/AxisBuilder.java
+++ src/jat/jat3D/AxisBuilder.java
@@ -1,3 +1,25 @@
+/* JAT: Java Astrodynamics Toolkit
+ * 
+  Copyright 2012 Tobias Berthold
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+// Original Code under LGPL
+// http://java.freehep.org/freehep-java3d/license.html
+
+
+
 package jat.jat3D;
 import java.awt.Font;
 import javax.media.j3d.*;
