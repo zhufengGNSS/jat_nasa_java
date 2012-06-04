fdd929fb8e0fa210b0db15409fd5a0b1841d917f
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-06-04 17:51:12 +0000

    bounding box axes

--- trunk/src/jat/jat3D/plot3D/JatPlot3D.java
+++ trunk/src/jat/jat3D/plot3D/JatPlot3D.java
@@ -84,7 +84,7 @@ public abstract class JatPlot3D extends Canvas3D {
 
 		// look at the right spot
 		Transform3D lookAt = new Transform3D();
-		lookAt.lookAt(new Point3d(1.5, 1.5, 1), new Point3d(0.0, 0.0, 0.0), new Vector3d(0, 0, 1.0));
+		lookAt.lookAt(new Point3d(2, 2, 1), new Point3d(0.0, 0.0, 0.0), new Vector3d(0, 0, 1.0));
 		lookAt.invert();
 		myvp.getViewPlatformTransform().setTransform(lookAt);
 
