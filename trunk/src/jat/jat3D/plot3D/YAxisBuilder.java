e173dd7d2e2773768a7459c890cc2c3191860723
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-06-07 23:08:21 +0000

    bounding box axes

--- trunk/src/jat/jat3D/plot3D/YAxisBuilder.java
+++ trunk/src/jat/jat3D/plot3D/YAxisBuilder.java
@@ -42,7 +42,7 @@ public class YAxisBuilder extends AxisBuilder
 	public Node getNode()
 	{
 		Transform3D t3d = new Transform3D();
-		t3d.set(1/scale,new Vector3f(lo,-hi,lo));
+		t3d.set(1/scale,new Vector3f(lo,lo,lo));
 		Transform3D rot = new Transform3D();
 		rot.rotZ(Math.PI/2);
 		t3d.mul(rot);
