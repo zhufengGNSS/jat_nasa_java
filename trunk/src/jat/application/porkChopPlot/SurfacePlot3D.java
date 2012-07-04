9461d9396c4948ad53a32928daa7369a23839ace
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-07-04 01:42:29 +0000

    pork chop plot

---
+++ trunk/src/jat/application/porkChopPlot/SurfacePlot3D.java
@@ -0,0 +1,177 @@
+/* JAT: Java Astrodynamics Toolkit
+ * 
+  Copyright 2012 Tobias Berthold
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package jat.application.porkChopPlot;
+
+import jat.jat3D.BodyGroup3D;
+import jat.jat3D.ColorCube3D;
+import jat.jat3D.Marker3D;
+import jat.jat3D.jatScene3D;
+import jat.jat3D.behavior.jat_KeyBehavior_Translate;
+import jat.jat3D.plot3D.BoundingBox3D;
+import jat.jat3D.plot3D.JatPlot3D;
+import jat.jat3D.plot3D.NormalizedBinned2DData;
+import jat.jat3D.plot3D.NormalizedBinned2DLogData;
+import jat.jat3D.plot3D.SurfaceBuilder;
+
+import java.io.IOException;
+
+import javax.media.j3d.Group;
+import javax.media.j3d.Node;
+import javax.media.j3d.Switch;
+import javax.swing.JOptionPane;
+
+public class SurfacePlot3D extends JatPlot3D {
+	private static final long serialVersionUID = 4491485448719846256L;
+	private pcplot_Jat3D_main main;
+	public pcplot_Jat3D_Data pcplot_data;
+	NormalizedBinned2DData ndata;
+	public SurfaceBuilder builder;
+	public Node plot;
+	public boolean logZscaling = false;
+	public Switch flightSelectorSwitch;
+	public Marker3D m;
+	public pcplot_jat3D_KeyBehavior keyBehavior_u;
+
+	public SurfacePlot3D(pcplot_Jat3D_main main) {
+		super();
+		initialViewingPosition.x = -.5;
+		initialViewingPosition.y = -1;
+		initialViewingPosition.z = 2;
+		this.main = main;
+	}
+
+	public Node createScene() {
+		Group g = new Group();
+		jatScene = new jatScene3D();
+
+		try {
+			pcplot_data = new pcplot_Jat3D_Data();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, "DE405 Ephemeris data file not found.");
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+
+		main.pcpGUI.depart_date_picker.getModel().setYear(pcplot_data.dep_year);
+		main.pcpGUI.depart_date_picker.getModel().setMonth(pcplot_data.dep_month);
+		main.pcpGUI.depart_date_picker.getModel().setDay(pcplot_data.dep_day);
+		main.pcpGUI.depart_date_picker.getModel().setSelected(true);
+
+		main.pcpGUI.arrival_date_picker.getModel().setYear(pcplot_data.arr_year);
+		main.pcpGUI.arrival_date_picker.getModel().setMonth(pcplot_data.arr_month);
+		main.pcpGUI.arrival_date_picker.getModel().setDay(pcplot_data.arr_day);
+		main.pcpGUI.arrival_date_picker.getModel().setSelected(true);
+
+		builder = new SurfaceBuilder();
+
+
+		if (logZscaling)
+			plot = builder.buildContent(new NormalizedBinned2DLogData(pcplot_data));
+		else {
+			ndata = new NormalizedBinned2DData(pcplot_data);
+			plot = builder.buildContent(ndata);
+		}
+		float boxsize = 1;
+
+		// setData(pcplot_data);
+		jatScene.InitialTranslation.x = boxsize / 2;
+		jatScene.InitialTranslation.y = boxsize / 2;
+		jatScene.addChild(plot);
+
+		viewingCenter.x = boxsize / 2;
+		viewingCenter.y = boxsize / 2;
+		viewingCenter.z = boxsize / 2;
+
+		// RGBAxes3D r = new RGBAxes3D();
+		// jatScene.addChild(r);
+
+		g.addChild(jatScene);
+
+		// initial zoom: exponent of ten times graph unit
+		exponent = 0;
+		bbox = new BoundingBox3D(0, 1);
+		bbox.xAxisLabel = "Dep " + pcplot_data.p.A.RowLabels[0] + " to "
+				+ pcplot_data.p.A.RowLabels[pcplot_data.xBins() - 1];
+		bbox.yAxisLabel = "Arr " + pcplot_data.p.A.ColumnLabels[0] + " to "
+				+ pcplot_data.p.A.ColumnLabels[pcplot_data.xBins() - 1];
+		bbox.zAxisLabel = (int) pcplot_data.zMin()+" to " + (int) pcplot_data.zMax()+" km/s";
+		bbox.createAxes(exponent);
+		bboxgroup = new BodyGroup3D(bbox, "Box");
+
+		g.addChild(bboxgroup);
+
+		flightSelectorSwitch = new Switch();
+		flightSelectorSwitch.setCapability(Switch.ALLOW_SWITCH_WRITE);
+		flightSelectorSwitch.addChild(new ColorCube3D(.00001));
+
+		m = new Marker3D();
+		flightSelectorSwitch.addChild(m);
+		flightSelectorSwitch.setWhichChild(0);
+		g.addChild(flightSelectorSwitch);
+
+		return g;
+	}
+
+	public void setData(pcplot_Jat3D_Data data) {
+		this.pcplot_data = data;
+
+		System.out.println(pcplot_data.p.A.ColumnLabels[0]);
+		bbox.xAxisLabel = "Dep " + pcplot_data.p.A.RowLabels[0] + " to "
+				+ pcplot_data.p.A.RowLabels[pcplot_data.xBins() - 1];
+		bbox.yAxisLabel = "Arr " + pcplot_data.p.A.ColumnLabels[0] + " to "
+				+ pcplot_data.p.A.ColumnLabels[pcplot_data.xBins() - 1];
+		bbox.zAxisLabel = (int) pcplot_data.zMin()+" to " + (int) pcplot_data.zMax()+" km/s";
+		bbox.setLabels(exponent);
+
+		if (logZscaling)
+			builder.updatePlot(new NormalizedBinned2DLogData(data));
+		else {
+			ndata = new NormalizedBinned2DData(pcplot_data);
+			builder.updatePlot(ndata);
+		}
+	}
+
+	public void addBehavior() {
+		jat_KeyBehavior_Translate keyBehavior_t = new jat_KeyBehavior_Translate(this);
+		keyBehavior_t.setSchedulingBounds(getDefaultBounds());
+		keyBehaviorSwitch.addChild(keyBehavior_t);
+		// pcplot_Jat3D_main main = null;
+		keyBehavior_u = new pcplot_jat3D_KeyBehavior(main);
+		keyBehavior_u.setSchedulingBounds(getDefaultBounds());
+		keyBehaviorSwitch.addChild(keyBehavior_u);
+	}
+
+	public boolean getLogZscaling() {
+		return logZscaling;
+	}
+
+	public void setLogZscaling(boolean b) {
+		// System.out.println("setting Log Scaling to: " + b + " from: " +
+		// logZscaling);
+		if (logZscaling != b) {
+			logZscaling = b;
+			if (pcplot_data != null) {
+				if (logZscaling)
+					builder.updatePlot(new NormalizedBinned2DLogData(pcplot_data));
+				else
+					builder.updatePlot(new NormalizedBinned2DData(pcplot_data));
+			}
+		}
+	}
+}
---
+++ trunk/src/jat/application/porkChopPlot/pcplot_Jat3D_GUI.java
@@ -0,0 +1,195 @@
+/* JAT: Java Astrodynamics Toolkit
+ * 
+  Copyright 2012 Tobias Berthold
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package jat.application.porkChopPlot;
+
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.border.TitledBorder;
+
+import net.sourceforge.jdatepicker.JDateComponentFactory;
+import net.sourceforge.jdatepicker.JDatePicker;
+import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
+
+public class pcplot_Jat3D_GUI extends JPanel {
+	private static final long serialVersionUID = 7907463395861359469L;
+	pcplot_Jat3D_Events pcpE;
+	private JPanel SearchIntervalPanel;
+	private JPanel MissionSelectPanel;
+	private JPanel DatesPanel;
+	private JPanel IntervalPanel;
+	private JPanel KeyboardPanel;
+	public JButton btn_make_plot;
+	private JLabel lbldepart;
+	private JLabel lblarrive;
+	JDatePicker depart_date_picker;
+	JDatePicker arrival_date_picker;
+	JDatePickerImpl datePicker1;
+	JSpinner spinner_days;
+	private JLabel lblDays;
+	JSpinner spinner_steps;
+	private JLabel lblSteps;
+	private JLabel lblDepartureDate;
+	public JFormattedTextField field_selected_departure_date;
+	private JLabel lblArrivalDate;
+	public JFormattedTextField field_selected_arrival_date;
+	private JLabel lblTotalDeltav;
+	public JFormattedTextField field_total_deltav;
+	public JRadioButton rdbtnPlotRotate;
+	public JRadioButton rdbtnFlightSelect;
+	public JButton btnGoMin;
+	private JLabel lblNewLabel;
+	public JButton btnStep;
+
+	public pcplot_Jat3D_GUI() {
+		pcpE = new pcplot_Jat3D_Events(this);
+		SearchIntervalPanel = new JPanel();
+		MissionSelectPanel = new JPanel();
+		DatesPanel = new JPanel();
+		IntervalPanel = new JPanel();
+		DatesPanel.setLayout(new GridLayout(5, 1, 5, 5));
+		IntervalPanel.setLayout(new GridLayout(2, 2, 5, 5));
+		KeyboardPanel = new JPanel();
+
+		SearchIntervalPanel.setBorder(new TitledBorder(null, "Search Interval", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		MissionSelectPanel.setBorder(new TitledBorder(null, "Flight Selection", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		KeyboardPanel.setBorder(new TitledBorder(null, "Keyboard Mode", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		depart_date_picker = JDateComponentFactory.createJDatePicker();
+		//depart_date_picker = (JDatePickerImpl) JDateComponentFactory.createJDatePicker();
+		depart_date_picker.setTextEditable(true);
+		depart_date_picker.setShowYearButtons(true);
+		//depart_date_picker.getModel().setYear(2012);
+		//depart_date_picker.getModel().setSelected(true);
+
+		arrival_date_picker = JDateComponentFactory.createJDatePicker();
+		depart_date_picker.setTextEditable(true);
+		depart_date_picker.setShowYearButtons(true);
+
+		add(SearchIntervalPanel);
+
+		SearchIntervalPanel.setLayout(new BoxLayout(SearchIntervalPanel, BoxLayout.Y_AXIS));
+		SearchIntervalPanel.add(DatesPanel);
+		SearchIntervalPanel.add(IntervalPanel);
+
+		lbldepart = new JLabel("Departure");
+		DatesPanel.add(lbldepart);
+		DatesPanel.add((JComponent) depart_date_picker);
+
+		lblarrive = new JLabel("Earliest Arrival");
+		DatesPanel.add(lblarrive);
+
+		DatesPanel.add((JComponent) arrival_date_picker);
+		
+		lblNewLabel = new JLabel("Interval");
+		DatesPanel.add(lblNewLabel);
+
+		spinner_days = new JSpinner();
+		IntervalPanel.add(spinner_days);
+		spinner_days.setModel(new SpinnerNumberModel(new Integer(500), null, null, new Integer(1)));
+
+		lblDays = new JLabel("Days");
+		IntervalPanel.add(lblDays);
+
+		spinner_steps = new JSpinner();
+		IntervalPanel.add(spinner_steps);
+		spinner_steps.setModel(new SpinnerNumberModel(new Integer(10), null, null, new Integer(1)));
+
+		lblSteps = new JLabel("Steps");
+		IntervalPanel.add(lblSteps);
+
+		btn_make_plot = new JButton("Make Plot");
+		SearchIntervalPanel.add(btn_make_plot);
+
+		btn_make_plot.addActionListener(pcpE);
+		
+		add(KeyboardPanel);
+		KeyboardPanel.setLayout(new BoxLayout(KeyboardPanel, BoxLayout.Y_AXIS));
+		
+		rdbtnPlotRotate = new JRadioButton("Plot Rotate/Zoom");
+		rdbtnPlotRotate.setSelected(true);
+		KeyboardPanel.add(rdbtnPlotRotate);
+		
+		rdbtnFlightSelect = new JRadioButton("Flight Select");
+		KeyboardPanel.add(rdbtnFlightSelect);
+
+		// Group the radio buttons.
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnPlotRotate);
+		group.add(rdbtnFlightSelect);
+		
+		
+		add(MissionSelectPanel);
+		// MissionSelectPanel.setLayout(new BoxLayout(MissionSelectPanel,
+		// BoxLayout.Y_AXIS));
+		MissionSelectPanel.setLayout(new GridLayout(8, 1));
+
+		lblDepartureDate = new JLabel("Departure Date");
+		MissionSelectPanel.add(lblDepartureDate);
+
+		field_selected_departure_date = new JFormattedTextField();
+
+		// field_selected_departure_date.setMaximumSize(maximumSize);
+		MissionSelectPanel.add(field_selected_departure_date);
+
+		lblArrivalDate = new JLabel("Arrival Date");
+		MissionSelectPanel.add(lblArrivalDate);
+
+		field_selected_arrival_date = new JFormattedTextField();
+		MissionSelectPanel.add(field_selected_arrival_date);
+
+		lblTotalDeltav = new JLabel("Total DeltaV");
+		MissionSelectPanel.add(lblTotalDeltav);
+
+		field_total_deltav = new JFormattedTextField();
+		MissionSelectPanel.add(field_total_deltav);
+		
+		btnGoMin = new JButton("Go To Minimum");
+		btnGoMin.setEnabled(false);
+		MissionSelectPanel.add(btnGoMin);
+		btnGoMin.addActionListener(pcpE);
+		
+		btnStep = new JButton("Step");
+		btnStep.setEnabled(false);
+		MissionSelectPanel.add(btnStep);
+		btnStep.addActionListener(pcpE);
+		
+		rdbtnPlotRotate.addActionListener(pcpE);
+		rdbtnFlightSelect.addActionListener(pcpE);
+	}
+
+	// private void JCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {
+	// plot.setParallelProjection(!jCheckBox2.isSelected());
+	// }
+	//
+	// private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {
+	// surf.setLogZscaling(jCheckBox1.isSelected());
+	// }
+
+}
