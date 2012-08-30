a79e15d0089316c1b174c26816598a2dd33f116a
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-08-30 21:04:12 +0000

    mission planner

--- src/jat/application/porkChopPlot/PorkChopPlot_KeyBehavior.java
+++ src/jat/application/porkChopPlot/PorkChopPlot_KeyBehavior.java
@@ -33,7 +33,7 @@ public class PorkChopPlot_KeyBehavior extends jat_KeyBehavior_UserCall {
 	}
 
 	public void keyup() {
-		if (y_index < main.surf.pcplot_data.p.steps - 1) {
+		if (y_index < main.pcpPlot.pcplot_data.p.steps - 1) {
 			// r.y += main.surf.pcplot_data.p.step_size;
 			y_index++;
 			updateMarker();
@@ -50,7 +50,7 @@ public class PorkChopPlot_KeyBehavior extends jat_KeyBehavior_UserCall {
 
 	public void keyright() {
 		// System.out.println("right");
-		if (x_index < main.surf.pcplot_data.p.steps - 1) {
+		if (x_index < main.pcpPlot.pcplot_data.p.steps - 1) {
 			// r.x += main.surf.pcplot_data.p.step_size;
 			x_index++;
 			updateMarker();
@@ -67,21 +67,23 @@ public class PorkChopPlot_KeyBehavior extends jat_KeyBehavior_UserCall {
 	}
 
 	public void updateMarker() {
-		float step_size = main.surf.pcplot_data.p.step_size;
+		float step_size = main.pcpPlot.pcplot_data.p.step_size;
 		r.x = x_index * step_size + step_size / 2;
 		r.y = y_index * step_size + step_size / 2;
-		r.z = main.surf.ndata.zAt(x_index, y_index);
-		main.surf.m.set_position(r);
+		r.z = main.pcpPlot.ndata.zAt(x_index, y_index);
+		main.pcpPlot.m.set_position(r);
 		// System.out.println(x_index + " " + y_index + " " + r.z);
-		main.pcpGUI.field_total_deltav.setText("" + main.surf.pcplot_data.zAt(x_index, y_index));
-		main.pcpGUI.field_selected_departure_date.setText(main.surf.pcplot_data.p.A.RowLabels[x_index]);
-		main.pcpGUI.field_selected_arrival_date.setText(main.surf.pcplot_data.p.A.ColumnLabels[y_index]);
+		main.pcpGUI.field_total_deltav.setText("" + main.pcpPlot.pcplot_data.zAt(x_index, y_index));
+		main.pcpGUI.field_selected_departure_date.setText(main.pcpPlot.pcplot_data.p.A.RowLabels[x_index]);
+		main.pcpGUI.field_selected_arrival_date.setText(main.pcpPlot.pcplot_data.p.A.ColumnLabels[y_index]);
 
+		// Keep selected dates as return value if package used in dialog
 		System.out.print("Dep ");
-		new TimeAPL(main.surf.pcplot_data.p.DepartureDate[x_index]).print();
+		new TimeAPL(main.pcpPlot.pcplot_data.p.DepartureDate[x_index]).print();
 		System.out.print("Arr ");
-		new TimeAPL(main.surf.pcplot_data.p.ArrivalDate[y_index]).print();
-
+		new TimeAPL(main.pcpPlot.pcplot_data.p.ArrivalDate[y_index]).print();
+		main.pReturn.DepartureDate = main.pcpPlot.pcplot_data.p.DepartureDate[x_index];
+		main.pReturn.ArrivalDate = main.pcpPlot.pcplot_data.p.ArrivalDate[y_index];
 	}
 
 	public void reset() {
@@ -90,6 +92,6 @@ public class PorkChopPlot_KeyBehavior extends jat_KeyBehavior_UserCall {
 		r.y = 0;
 		x_index = 0;
 		y_index = 0;
-		main.surf.m.set_position(r);
+		main.pcpPlot.m.set_position(r);
 	}
 }
