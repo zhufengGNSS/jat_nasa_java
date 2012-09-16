12da62d5c468b863818581ec6e90ae3c84bd8027
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-09-16 23:52:52 +0000

    mission planner

---
+++ trunk/src/jat/core/ephemeris/DE405_Enum_APL.java
@@ -0,0 +1,30 @@
+/* JAT: Java Astrodynamics Toolkit
+ * 
+  Copyright 2012 Tobias Berthold
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package jat.core.ephemeris;
+
+public class DE405_Enum_APL {
+
+	public enum body {
+		MERCURY, VENUS, EARTH_MOON_BARY, MARS, JUPITER;
+	};
+
+	public static String[] name = { "MERCURY", "VENUS", "EARTH_MOON_BARY", "MARS", "JUPITER" };
+
+}
+
+// MERCURY(1), VENUS(2), EARTH_MOON_BARY(3), MARS(4), JUPITER(5);
