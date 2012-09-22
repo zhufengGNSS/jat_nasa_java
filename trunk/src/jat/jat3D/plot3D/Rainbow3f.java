a5a4fc8be80f1ebfb4d4b3f9eae5a1643487109e
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-09-22 02:43:10 +0000

    mission planner

--- trunk/src/jat/jat3D/plot3D/Rainbow3f.java
+++ trunk/src/jat/jat3D/plot3D/Rainbow3f.java
@@ -4,24 +4,25 @@ import javax.vecmath.Color3f;
 
 public class Rainbow3f {
 	private double min, max;
+	private static float r, g, b;
 	private static Color3f rtable[] = new Color3f[100];
 	static {
 		for (int j = 0; j < 40; j++) {
-			byte r = (byte) (255 * (1 - j / 40.0));
-			byte g = (byte) (255 * (j / 40.0));
-			byte b = 0;
+			r = 1.f - j / 40.0f;
+			g = j / 40.0f;
+			b = 0;
 			rtable[j] = new Color3f(r, g, b);
 		}
 		for (int j = 40; j < 80; j++) {
-			byte r = 0;
-			byte g = (byte) (255 * (80 - j) / 40.0);
-			byte b = (byte) (255 * (j - 40) / 40.0);
+			r = 0;
+			g = (80 - j) / 40.0f;
+			b = (j - 40) / 40.0f;
 			rtable[j] = new Color3f(r, g, b);
 		}
 		for (int j = 80; j < 100; j++) {
-			byte r = (byte) (255 * (j - 80) / 40.0);
-			byte g = 0;
-			byte b = (byte) (255 * (120 - j) / 40.0);
+			r = (j - 80) / 40.0f;
+			g = 0;
+			b = (120 - j) / 40.0f;
 			rtable[j] = new Color3f(r, g, b);
 		}
 	}
@@ -39,12 +40,12 @@ public class Rainbow3f {
 		return rtable[i];
 	}
 
-//	public Color3f colorFor(double d) {
-//		int i = (int) Math.floor((d - min) * 100 / (max - min));
-//		if (i < 0)
-//			i = 0;
-//		else if (i >= rtable.length)
-//			i = rtable.length - 1;
-//		return rtable[i];
-//	}
+	// public Color3f colorFor(double d) {
+	// int i = (int) Math.floor((d - min) * 100 / (max - min));
+	// if (i < 0)
+	// i = 0;
+	// else if (i >= rtable.length)
+	// i = rtable.length - 1;
+	// return rtable[i];
+	// }
 }
