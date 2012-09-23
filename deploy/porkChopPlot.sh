509d7c82efc7645df511d34c4fc77193eb5b99a9
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-09-23 01:32:29 +0000

    mission planner

--- deploy/porkChopPlot.sh
+++ deploy/porkChopPlot.sh
@@ -3,21 +3,19 @@
 
 
 export LD_LIBRARY_PATH=/usr/lib/jni
-
 echo $LD_LIBRARY_PATH
 
-JARDISTPATH="/media/data/workspace/jat/dist"
-JARLIBPATH="/media/data/workspace/jat/lib"
+#From within Eclipse:
+#JARDISTPATH="/media/data/workspace/jat/dist"
+#JARLIBPATH="/media/data/workspace/jat/lib"
+
+# From command line:
+JARDISTPATH="../dist"
+JARLIBPATH="../lib"
 
 pwd
 
-#=/home/user/Desktop/j3d-1_5_2-linux-i586/lib/i386
-
-
-#java -cp ../dist/jatapplication.jar:../dist/jatcore.jar:../lib/plot.jar:../lib/j3dcore.jar:../lib/j3dutils.jar:../lib/vecmath.jar:../lib/jdatepicker-1.3.2.jar   jat.application.porkChopPlot.pcplot_Jat3D_main
-
-#From within Eclipse:
-java -cp $JARDISTPATH/jatapplication.jar:$JARDISTPATH/jatcore.jar:$JARLIBPATH/j3dcore.jar:$JARLIBPATH/j3dutils.jar:$JARLIBPATH/vecmath.jar:$JARLIBPATH/jdatepicker-1.3.2.jar   jat.application.porkChopPlot.pcplot_Jat3D_main
+java -cp $JARDISTPATH/jatapplication.jar:$JARDISTPATH/jatcore.jar:$JARLIBPATH/j3dcore.jar:$JARLIBPATH/j3dutils.jar:$JARLIBPATH/vecmath.jar:$JARLIBPATH/jdatepicker-1.3.2.jar   jat.application.porkChopPlot.PorkChopPlot_main
 
 echo press enter
 
