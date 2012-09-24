1eb116153f6cf4427f679f133d10b8a0934db335
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-09-24 19:57:46 +0000

    mission planner

--- trunk/src/jat/jat3D/Ephemeris3D.java
+++ trunk/src/jat/jat3D/Ephemeris3D.java
@@ -17,11 +17,13 @@
 
 package jat.jat3D;
 
+import java.io.IOException;
+
 import jat.core.cm.Constants;
 import jat.core.cm.cm;
-import jat.core.ephemeris.DE405;
-import jat.core.ephemeris.DE405_Body;
+import jat.core.ephemeris.DE405APL;
 import jat.core.math.matvec.data.VectorN;
+import jat.core.spacetime.Time;
 import jat.core.spacetime.TimeUtils;
 import jat.core.util.FileUtil;
 import jat.core.util.PathUtil;
@@ -37,66 +39,68 @@ import javax.vecmath.Color3f;
  */
 public class Ephemeris3D extends Body3D {
 	float size;
-	DE405_Body body;
+	DE405APL myEph;
+	DE405APL.body body;
+	double jd;
 	int steps = 100;
 	public double[] coords;
 	Color3f Color = Colors.gray;
-	DE405 my_eph;
-	double jd;
 	Shape3D s;
-	//Matrix MRot;
+	Time startTime;
 
-	public Ephemeris3D(DE405_Body bod, double jd_start, double jd_end) {
-		// super(myapplet);
-		this.body = bod;
-		this.jd = jd_start;
-		String fs = FileUtil.file_separator();
-		my_eph = new DE405(FileUtil.getClassFilePath("jat.eph", "DE405") + fs + "DE405data" + fs);
-		// MRot=new RotationMatrix(1,cm.Rad(Constants.eps));
-		draw();
-	}
+	// Matrix MRot;
 
-	public Ephemeris3D(DE405_Body bod, double jd_start, int days) {
-		// super(myapplet);
-		this.body = bod;
-		this.jd = jd_start;
+	// public Ephemeris3D(DE405APL.body body, double jd_start, double jd_end) {
+	// this.body = body;
+	// this.jd = jd_start;
+	// String fs = FileUtil.file_separator();
+	// my_eph = new DE405(FileUtil.getClassFilePath("jat.eph", "DE405") + fs +
+	// "DE405data" + fs);
+	// // MRot=new RotationMatrix(1,cm.Rad(Constants.eps));
+	// draw();
+	// }
+
+	public Ephemeris3D(DE405APL myEph, DE405APL.body body, Time startTime, int days) {
+		this.myEph = myEph;
+		this.body = body;
+		this.startTime = startTime;
 		this.steps = days;
 
-		PathUtil f = new PathUtil();
-		String fs = FileUtil.file_separator();
-		String DE405_data_folder = f.root_path + "data" + fs + "core" + fs + "ephemeris" + fs + "DE405data" + fs;
-		my_eph = new DE405(DE405_data_folder);
-		//MRot = new RotationMatrix(1, cm.Rad(Constants.eps));
+		// PathUtil f = new PathUtil();
+		// String fs = FileUtil.file_separator();
+		// String DE405_data_folder = f.root_path + "data" + fs + "core" + fs +
+		// "ephemeris" + fs + "DE405data" + fs;
+		// my_eph = new DE405(DE405_data_folder);
+		// MRot = new RotationMatrix(1, cm.Rad(Constants.eps));
 		draw();
 	}
 
 	private void draw() {
 		// double rv[];
 		VectorN r;
+		Time time = startTime;
 		// Create coords array
 		coords = new double[steps * 3];
 		for (int k = 0; k < steps; k++) {
-			double mjd_tt = TimeUtils.JDtoMJD(jd);
+			// double mjd_tt = TimeUtils.JDtoMJD(jd);
 			// rv = MRot.times(new VectorN(my_eph.get_planet_pos(body,
 			// mjd_tt+k)));
-			r = new VectorN(my_eph.get_planet_pos(body, mjd_tt + k));
-			// System.out.println("The position is");
-			// System.out.println("x= " + rv[0] + " km");
-			// System.out.println("y= " + rv[1] + " km");
-			// System.out.println("z= " + rv[2] + " km");
-			// coords[k * 3 + 0] = rv[0];
-			// coords[k * 3 + 1] = rv[1];
-			// coords[k * 3 + 2] = rv[1];
-			double x, y, z, eps, c, s;
-			x = r.get(0);
-			y = r.get(1);
-			z = r.get(2);
-			eps = cm.Rad(Constants.eps);
-			c = Math.cos(eps);
-			s = Math.sin(eps);
-			coords[k * 3 + 0] = x;
-			coords[k * 3 + 1] = c*y+s*z;
-			coords[k * 3 + 2] = -s*y+c*z;
+			time.step_seconds(k *100);
+			try {
+				r = new VectorN(myEph.get_planet_pos(body, time));
+				double x, y, z, eps, c, s;
+				x = r.get(0);
+				y = r.get(1);
+				z = r.get(2);
+				eps = cm.Rad(Constants.eps);
+				c = Math.cos(eps);
+				s = Math.sin(eps);
+				coords[k * 3 + 0] = x;
+				coords[k * 3 + 1] = c * y + s * z;
+				coords[k * 3 + 2] = -s * y + c * z;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		int num_vert = coords.length / 3;
 		int[] stripLengths = { num_vert };
