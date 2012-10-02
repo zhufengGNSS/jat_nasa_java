aa0e947aad5be84ff55054824061ccf8ada3fafd
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-10-02 21:39:46 +0000

    reorganize

---
+++ trunk/src/jat/application/missionPlanRunLocal/Flight.java
@@ -0,0 +1,31 @@
+package jat.application.missionPlanRunLocal;
+
+import javax.vecmath.Color3f;
+
+import jat.core.cm.Lambert;
+import jat.core.ephemeris.DE405APL.body;
+import jat.core.math.matvec.data.VectorN;
+import jat.core.spacetime.TimeAPL;
+import jat.jat3D.Sphere3D;
+import jat.jat3D.TwoBodyOrbit3D;
+
+public class Flight {
+	String flightName;
+	Color3f color;
+	String satelliteName;
+	public double mu;
+	body departure_planet;
+	String departurePlanetName;
+	String arrivalPlanetName;
+	body arrival_planet;
+	TimeAPL departureDate;
+	TimeAPL arrivalDate;
+	double tof;
+	Lambert lambert;
+	VectorN r0, v0, rf, vf, dv0, dvf;
+	public double totaldv;
+	double t0_on_orbit;
+	TwoBodyOrbit3D orbit;
+	Sphere3D satellite;
+
+}
