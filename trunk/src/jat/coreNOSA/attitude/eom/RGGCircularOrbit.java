7da3e26a8cfd446f3d0adc0a6a143419ab7754ce
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-10-09 01:56:10 +0000

    reorganize

--- trunk/src/jat/coreNOSA/attitude/eom/RGGCircularOrbit.java
+++ trunk/src/jat/coreNOSA/attitude/eom/RGGCircularOrbit.java
@@ -20,7 +20,6 @@
 package jat.coreNOSA.attitude.eom;
  
 import jat.core.algorithm.integrators.*;
-import jat.core.attitude.*;
 import jat.core.math.matvec.data.*;
 import jat.coreNOSA.attitude.QuatToDeg;
 import jat.coreNOSA.plotutil.FourPlots;
