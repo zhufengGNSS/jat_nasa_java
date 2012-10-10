0c74e328ad101aa7fea7c65c4c951952931b7c69
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-10-10 06:02:23 +0000

    tests applets

--- trunk/deploy/AttitudeSimulator.sh
+++ trunk/deploy/AttitudeSimulator.sh
@@ -5,7 +5,7 @@ export LD_LIBRARY_PATH=/usr/lib/jni
 
 ECHO $LD_LIBRARY_PATH=/home/user/Desktop/j3d-1_5_2-linux-i586/lib/i386
 
-java -cp ../dist/jatapplication.jar:../dist/jatcore.jar:../lib/plot.jar:../lib/j3dcore.jar:../lib/j3dutils.jar:../lib/vecmath.jar   jat.application.AttitudeSimulator.AttitudeSimulator
+java -cp ../dist/jatapplication.jar:../dist/jatcore.jar:../dist/jatcoreNOSA.jar:../lib/plot.jar:../lib/j3dcore.jar:../lib/j3dutils.jar:../lib/vecmath.jar   jat.application.AttitudeSimulator.AttitudeSimulator
 
 echo press enter
 
