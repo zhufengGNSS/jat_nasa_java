bb252ef226277d9c47187df09ead97ccf420d6b3
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-10-13 01:28:12 +0000

    new APL applets

--- src/jat/examples/TwoBodyExample/TwoBodyExample.java
+++ src/jat/examples/TwoBodyExample/TwoBodyExample.java
@@ -24,11 +24,13 @@ import jat.core.plot.plot.PlotPanel;
 import jat.core.plot.plot.plots.ScatterPlot;
 
 import java.awt.Color;
+import java.text.DecimalFormat;
 
 import javax.swing.JFrame;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealVectorFormat;
 import org.apache.commons.math3.ode.FirstOrderIntegrator;
 import org.apache.commons.math3.ode.nonstiff.DormandPrince853Integrator;
 
@@ -38,28 +40,20 @@ public class TwoBodyExample {
 	}
 
 	public static void main(String[] args) {
+
 		TwoBodyExample x = new TwoBodyExample();
 
 		// create a TwoBody orbit using orbit elements
-		TwoBodyAPL sat = new TwoBodyAPL(7000.0, 0.9, 0.0, 0.0, 0.0, 0.0);
+		TwoBodyAPL sat = new TwoBodyAPL(7000.0, 0.3, 0.0, 0.0, 0.0, 0.0);
 
-        double [] y = sat.randv();
+		double[] y = sat.randv();
 
-        ArrayRealVector v=new ArrayRealVector(y);
-        
-        System.out.print(v.getEntry(0));
-        System.out.print(" ");
-        System.out.print(v.getEntry(1));
-        System.out.print(" ");
-        System.out.print(v.getEntry(2));
-        System.out.print(" ");
-        System.out.print(v.getEntry(3));
-        System.out.print(" ");
-        System.out.print(v.getEntry(4));
-        System.out.print(" ");
-        System.out.print(v.getEntry(5));
-        System.out.print(" ");
-    
+		ArrayRealVector v = new ArrayRealVector(y);
+
+		DecimalFormat df2 = new DecimalFormat("#,###,###,##0.00");
+		RealVectorFormat format = new RealVectorFormat(df2);
+		System.out.println(format.format(v));
+
 		// find out the period of the orbit
 		double period = sat.period();
 
@@ -72,11 +66,12 @@ public class TwoBodyExample {
 		// propagate the orbit
 		FirstOrderIntegrator dp853 = new DormandPrince853Integrator(1.0e-8, 100.0, 1.0e-10, 1.0e-10);
 		dp853.addStepHandler(sat.stepHandler);
-		//double[] y = new double[] { 7000.0, 0, 0, .0, 8, 0 }; // initial state
+		// double[] y = new double[] { 7000.0, 0, 0, .0, 8, 0 }; // initial
+		// state
 
-		dp853.integrate(sat, 0.0, y, 8000, y); // now y contains final state at tf		
+		dp853.integrate(sat, 0.0, y, 8000, y); // now y contains final state at
+												// tf
 
-		
 		Double[] objArray = sat.time.toArray(new Double[sat.time.size()]);
 		double[] timeArray = ArrayUtils.toPrimitive(objArray);
 		double[] xsolArray = ArrayUtils.toPrimitive(sat.xsol.toArray(new Double[sat.time.size()]));
@@ -92,21 +87,25 @@ public class TwoBodyExample {
 			XY[i][0] = xsolArray[i];
 			XY[i][1] = ysolArray[i];
 		}
+
 		Plot2DPanel p = new Plot2DPanel();
 
-		ScatterPlot s=new ScatterPlot("orbit", Color.RED, XY);
-		//LinePlot l = new LinePlot("sin", Color.RED, XY);
-		//l.closed_curve = false;
-		//l.draw_dot = true;
+		// Plot2DPanel p = new Plot2DPanel(min, max, axesScales, axesLabels);
+
+		ScatterPlot s = new ScatterPlot("orbit", Color.RED, XY);
+		// LinePlot l = new LinePlot("sin", Color.RED, XY);
+		// l.closed_curve = false;
+		// l.draw_dot = true;
 		p.addPlot(s);
 		p.setLegendOrientation(PlotPanel.SOUTH);
+		double plotSize = 10000.;
+		double[] min = { -plotSize, -plotSize };
+		double[] max = { plotSize, plotSize };
+		p.setFixedBounds(min, max);
 
 		new FrameView(p).setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		System.out.println("end");
-		
-		
-		
-		
+
 	}
 }
