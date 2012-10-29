3139d9a356961fe7adf70de5456ed8f002dad2b3
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-10-29 21:55:41 +0000

    earth moon

--- trunk/src/jat/examples/CRTBP/CRTBPExample.java
+++ trunk/src/jat/examples/CRTBP/CRTBPExample.java
@@ -39,7 +39,8 @@ public class CRTBPExample {
 	void doExample()
 	{
 		//double mu = 0.15;
-		double mu = 3.035909999e-6;
+		//double mu = 3.035909999e-6;
+		double mu=0.012277471;
 		//double mu = 0.3;
 		CRTBP myCRTBP = new CRTBP(mu);
 		FirstOrderIntegrator dp853 = new DormandPrince853Integrator(1.0e-8, 100.0, 1.0e-10, 1.0e-10);
