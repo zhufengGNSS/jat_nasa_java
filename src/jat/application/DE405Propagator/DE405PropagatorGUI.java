68bbae04fc3c15d9880a12b6eb2c5a23dea1fec8
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-11-10 01:38:34 +0000

    DE405 integrator application

--- src/jat/application/DE405Propagator/DE405PropagatorGUI.java
+++ src/jat/application/DE405Propagator/DE405PropagatorGUI.java
@@ -18,6 +18,7 @@
 package jat.application.DE405Propagator;
 
 import jat.core.ephemeris.DE405Body;
+import jat.core.ephemeris.DE405Body.body;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
@@ -49,6 +50,11 @@ public class DE405PropagatorGUI extends JPanel {
 	public JButton btnPlot;
 	JDatePicker depart_date_picker;
 	public JCheckBox chckbxMercury, chckbxEarth, chckbxJupiter;
+	public JCheckBox chckbxVenus;
+	public JCheckBox chckbxMars;
+	public JCheckBox chckbxSaturn;
+	public JCheckBox chckbxSun;
+	public JCheckBox chckbxMoon;
 
 	public DE405PropagatorGUI(DE405PropagatorMain dpMain) {
 		this.dpMain = dpMain;
@@ -76,21 +82,37 @@ public class DE405PropagatorGUI extends JPanel {
 		gbc_panelPlanets.gridx = 0;
 		gbc_panelPlanets.gridy = 0;
 		level1_Pane.add(panelPlanets, gbc_panelPlanets);
-		panelPlanets.setBorder(new TitledBorder(null, "Planet On/Off", TitledBorder.LEADING, TitledBorder.TOP, null,
-				null));
+		panelPlanets.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Body On/Off",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panelPlanets.setLayout(new GridLayout(0, 1, 0, 0));
+
+		chckbxSun = new JCheckBox("Sun");
+		panelPlanets.add(chckbxSun);
 
 		chckbxMercury = new JCheckBox("Mercury");
 		panelPlanets.add(chckbxMercury);
 
+		chckbxVenus = new JCheckBox("Venus");
+		panelPlanets.add(chckbxVenus);
+
 		chckbxEarth = new JCheckBox("Earth");
 		panelPlanets.add(chckbxEarth);
+
+		chckbxMoon = new JCheckBox("Moon");
+		panelPlanets.add(chckbxMoon);
+
+		chckbxMars = new JCheckBox("Mars");
+		panelPlanets.add(chckbxMars);
 
 		chckbxJupiter = new JCheckBox("Jupiter");
 		panelPlanets.add(chckbxJupiter);
 
+		chckbxSaturn = new JCheckBox("Saturn");
+		panelPlanets.add(chckbxSaturn);
+
 		JPanel panelDate = new JPanel();
-		panelDate.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Start Date, Flight Time [s]", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelDate.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Start Date, Flight Time [s]",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		GridBagConstraints gbc_panelDate = new GridBagConstraints();
 		gbc_panelDate.fill = GridBagConstraints.BOTH;
 		gbc_panelDate.insets = new Insets(0, 0, 5, 0);
@@ -112,7 +134,7 @@ public class DE405PropagatorGUI extends JPanel {
 		level1_Pane.add(panelIC, gbc_panelIC);
 		panelIC.setBorder(new TitledBorder(null, "Initial Condition", TitledBorder.LEADING, TitledBorder.TOP, null,
 				null));
-		panelIC.setLayout(new GridLayout(10, 1, 0, 0));
+		panelIC.setLayout(new GridLayout(9, 1, 0, 0));
 
 		JLabel lblNewLabel = new JLabel("x y z [km]");
 		panelIC.add(lblNewLabel);
@@ -150,19 +172,22 @@ public class DE405PropagatorGUI extends JPanel {
 		level1_Pane.add(chckbxRotation, gbc_chckbxRotation);
 
 		chckbxRotation.addItemListener(dpE);
+		chckbxSun.addItemListener(dpE);
 		chckbxEarth.addItemListener(dpE);
+		chckbxMoon.addItemListener(dpE);
+		chckbxJupiter.addItemListener(dpE);
 		btnPlot.addActionListener(dpE);
 	}
 
 	public void updateGUI() {
-
-		if (dpMain.dpParam.bodyGravOnOff[DE405Body.body.MERCURY.ordinal()])
-			chckbxMercury.setSelected(true);
-		if (dpMain.dpParam.bodyGravOnOff[DE405Body.body.EARTH.ordinal()])
-			chckbxEarth.setSelected(true);
-		if (dpMain.dpParam.bodyGravOnOff[DE405Body.body.JUPITER.ordinal()])
-			chckbxJupiter.setSelected(true);
-
+		chckbxSun.setSelected(dpMain.dpParam.bodyGravOnOff[body.SUN.ordinal()]);
+		chckbxMercury.setSelected(dpMain.dpParam.bodyGravOnOff[body.MERCURY.ordinal()]);
+		chckbxVenus.setSelected(dpMain.dpParam.bodyGravOnOff[body.VENUS.ordinal()]);
+		chckbxEarth.setSelected(dpMain.dpParam.bodyGravOnOff[body.EARTH.ordinal()]);
+		chckbxMoon.setSelected(dpMain.dpParam.bodyGravOnOff[body.MOON.ordinal()]);		
+		chckbxMars.setSelected(dpMain.dpParam.bodyGravOnOff[body.MARS.ordinal()]);
+		chckbxJupiter.setSelected(dpMain.dpParam.bodyGravOnOff[body.JUPITER.ordinal()]);
+		chckbxSaturn.setSelected(dpMain.dpParam.bodyGravOnOff[body.SATURN.ordinal()]);
 		
 		depart_date_picker.getModel().setYear(dpMain.dpParam.simulationDate.getYear());
 		depart_date_picker.getModel().setMonth(dpMain.dpParam.simulationDate.getMonth());
