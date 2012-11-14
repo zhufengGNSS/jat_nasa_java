9e175e1b89303e8d497e25b8b5e5c67e927ee264
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-11-14 21:58:43 +0000

    DE405 integrator application and unit check

--- src/jat/application/DE405Propagator/scenario/testOrbit.java
+++ src/jat/application/DE405Propagator/scenario/testOrbit.java
@@ -5,28 +5,28 @@ import jat.core.ephemeris.DE405Body.body;
 import jat.core.ephemeris.DE405Frame.frame;
 import jat.core.spacetime.TimeAPL;
 
-public class earthOrbitECI extends DE405PropagatorParameters {
+public class testOrbit  extends DE405PropagatorParameters{
 
-	public earthOrbitECI() {
+	public testOrbit() {
 		super();
 
-//		bodyGravOnOff[body.SUN.ordinal()] = true;
-		bodyGravOnOff[body.EARTH.ordinal()] = true;
-	//	bodyGravOnOff[body.MOON.ordinal()] = true;
-
-		Frame=frame.ECI;
-
+		bodyGravOnOff[body.SUN.ordinal()] = true;
+//		bodyGravOnOff[body.MARS.ordinal()] = true;
+//		bodyGravOnOff[body.JUPITER.ordinal()] = true;
+		
+		Frame=frame.HEE;
+//		Frame=frame.ICRF;
 		simulationDate = new TimeAPL(2003, 3, 1, 12, 0, 0);
-
-		// earth orbit
-		y0[0] = 150;
+	
+		y0[0] = 150.;
 		y0[1] = 0;
 		y0[2] = 0;
-		y0[3] = 0;
-		y0[4] = 10;
-		y0[5] = 0;
-		tf = 4.;
-
+		y0[4] = 40;
+		tf = 10.;
+	
 	}
 
+
+	
+	
 }
