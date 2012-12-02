e3d1f76302dd3a78b30156cd7d9076f528444ba4
tberthold <tberthold@a9f66c71-d4f8-4288-aa7b-a045fd4810de>
2012-12-02 02:44:43 +0000

    DE405 integrator application

--- trunk/src/jat/application/DE405Propagator/DE405PropagatorParameters.java
+++ trunk/src/jat/application/DE405Propagator/DE405PropagatorParameters.java
@@ -2,10 +2,7 @@ package jat.application.DE405Propagator;
 
 import jat.core.ephemeris.DE405Body.body;
 import jat.core.ephemeris.DE405Frame.frame;
-import jat.core.ephemeris.DE405Plus;
 import jat.core.spacetime.TimeAPL;
-import jat.core.util.PathUtil;
-import jat.core.util.jatMessages;
 
 public class DE405PropagatorParameters {
 	public String Name;
@@ -13,15 +10,11 @@ public class DE405PropagatorParameters {
 	int numberOfBodies = body.values().length;
 	boolean[] showPlanet = new boolean[numberOfBodies];
 	public boolean[] bodyGravOnOff = new boolean[numberOfBodies];
-	PathUtil path;
-	jatMessages messages;
-	DE405Plus Eph;
 	public frame Frame;
 	public double[] y0=new double[6] ; // initial position and velocity
 	public double tf;
 
 	public DE405PropagatorParameters() {
-		messages = new jatMessages();
 
 		for (int i = 0; i < numberOfBodies; i++) {
 			this.showPlanet[i] = true;
